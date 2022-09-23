d052b187cd881f95aebed962a679d9177f9e9e2c
ZAY <zaymyo.tun27@gmail.com>
2022-09-23 20:04:37 +0900

    store numbers in variables

--- Player.gd
+++ Player.gd
@@ -2,6 +2,13 @@ extends KinematicBody2D
 
 var velocity = Vector2.ZERO
 var fast_fell = false
+export(int) var JUMP_FORCE = -130
+export(int) var JUMP_RELEASE_FORCE = -70
+export(int) var MAX_SPEED = 50
+export(int) var ACCELERATION = 10
+export(int) var FRICTION = 10
+export(int) var GRAVITY = 4
+export(int) var ADDITIONAL_FALL_GRAVITY = 4
 
 func _ready():
 	pass # Replace with function body.
@@ -18,21 +25,21 @@ func _physics_process(delta):
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
-			velocity.y = -130
+			velocity.y = JUMP_FORCE
 	else:
-		if Input.is_action_just_released("ui_up") and velocity.y < -70:
-			velocity.y = -70
+		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_RELEASE_FORCE:
+			velocity.y = JUMP_RELEASE_FORCE
 		
 		if velocity.y > 0:
-			velocity.y += 4
+			velocity.y += ADDITIONAL_FALL_GRAVITY
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
 func apply_gravity():
-	velocity.y += 4
+	velocity.y += GRAVITY
 
 func apply_friction():
-	velocity.x = move_toward(velocity.x, 0, 10)
+	velocity.x = move_toward(velocity.x, 0, FRICTION)
 
 func apply_acceleration(amount):
-	velocity.x = move_toward(velocity.x, 50 * amount, 10)
+	velocity.x = move_toward(velocity.x, MAX_SPEED * amount, ACCELERATION)
