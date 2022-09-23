431fa09b8ff6da2774d4a8635c5ee8e7546e6981
ZAY <zaymyo.tun27@gmail.com>
2022-09-23 18:59:31 +0900

    implement fast fall when mid air

--- Player.gd
+++ Player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 var velocity = Vector2.ZERO
+var fast_fell = false
 
 func _ready():
 	pass # Replace with function body.
@@ -21,6 +22,9 @@ func _physics_process(delta):
 	else:
 		if Input.is_action_just_released("ui_up") and velocity.y < -70:
 			velocity.y = -70
+		
+		if velocity.y > 0:
+			velocity.y += 4
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
